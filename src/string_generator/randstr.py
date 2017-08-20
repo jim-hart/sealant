ceab0b4b00d75a03c1caef405b4a0dbce4d3966f
James <jim-hart@users.noreply.github.com>
2017-08-20 03:11:56 -0400

    Proper docstrings and RandomString refactor
    Expanded docstrings where detail was missing, and added docstrings if missing for all randstr files.
    
    RandomString() class was refactored partly to allow it to function independently, and for code cleanup purposes.  Prior to this branch, RandomString() was unintentionally written to rely on RandstrParser to get its arguments.  RandomString is meant to be used by itself if desired, so some changes were needed.  Default arguments were set for some of the __init__ parameters to account for this need, for instance.
    
    RandonString.shuffle_characters was changed from a static method to an instance method.  Instead of returning a shuffle character set, it modifies the self.char_set attribute directly.  self.generate_random_string() did this already, but this should be handled by self.shuffle_characters() directly.
    
    RandomStrings __repr__ override was changed to __str__.  This change doesn't cause any functionality change; it was done in the name of clarity to indicate RandomString is intended to be printed.

--- src/string_generator/randstr.py
+++ src/string_generator/randstr.py
@@ -1,3 +1,5 @@
+"""Methods for generating cryptographically secure random strings."""
+
 from __future__ import print_function
 from six.moves import range
 import string
@@ -8,73 +10,87 @@ uses secrets module while all other versions use random.SystemRandom().
 Additional details can be found in README."""
 
 import random
+
 try:
     import secrets as RAND_METHOD
-
-    print('\nSystem Python Version >= 3.6 | Using secrets module')
 except ImportError:
     RAND_METHOD = random.SystemRandom()
-    print('\nSystem Python Version < 3.6 | Using random.SystemRandom')
-
 
 # -----------------------------------------------------------------------------
 
 
 class RandomString(object):
     """Class for generating random strings based on default, or user-defined
-    parameters"""
+    parameters
 
-    def __init__(self, length, shuffle, user_char_set):
-        """
-        Args:
-            length (int)  -- Defines length of randomized string.
-            shuffle (bool)-- If true, char_set is shuffled via
-                             self.shuffle_characters() method prior to
-                             randomization process.
-            char_set (str)-- String of characters to be used as sample
-                             population for randomization process.  Defaults to
-                             ASCII character space, uppercase, lowercase, and
-                             punctuation characters if no argument provided.
-        """
+    Args:
+        length (int): Desired length of randomized string.
+        shuffle (bool, optional): If True, self.char_set will be shuffled prior
+            to string generation.
+        user_char_set (str, optional): Character set that will replace character
+            set provided by self.default_char_set property.
 
+    Attributes:
+        length (int): Length of the randomly generated string.
+        shuffle (bool): If True, self.char_set will be shuffled prior to string
+            generation.
+        char_set (str): character set to be used as population sample for
+            randomization process.
+    """
+
+    def __init__(self, length, shuffle=False, user_char_set=None):
         self.length = length
         self.shuffle = shuffle
         self.char_set = user_char_set or self.default_char_set
 
-    def __repr__(self):
-        """Returns object instance itself as representation of randomized
-        string"""
+    def __str__(self):
+        """Returns object as a printable representation of itself
+
+        Returns:
+            str: string containing randomly chosen characters
+        """
 
         return self.generate_random_string()
 
     def generate_random_string(self):
-        """Returns a randomized string of either preset, or user-defined
-        length."""
+        """Builds randomized string based on instance attributes
 
-
+        Returns:
+            str: Random string of len:self.length built from self.char_set
+                characters
+        """
 
         if self.shuffle:
-            self.char_set = self.shuffle_characters(list(self.char_set))
+            self.shuffle_characters()
 
         return "".join(
             [RAND_METHOD.choice(self.char_set) for _ in range(0, self.length)])
 
-    @staticmethod
-    def shuffle_characters(char_set):
-        """Method for shuffling character set between 3-5 times.  Shuffle count
-        is randomly chosen."""
+    def shuffle_characters(self):
+        """Method for in-place shuffling of self.char_set between 3-5 times;
+        Shuffle count is randomly chosen.
 
-        shuffle_count = RAND_METHOD.choice(list(range(3, 6)))
+        Returns:
+            None: self.char_set attribute is modified directly once character
+                indices have been shuffled.
+        """
+
+        char_list = list(self.char_set)
+        shuffle_count = RAND_METHOD.choice(range(3, 6))
 
         for _ in range(0, shuffle_count):
-            random.shuffle(char_set)
+            random.shuffle(char_list)
 
-        return "".join(char_set)
+        self.char_set = ''.join(char_list)
 
     @property
     def default_char_set(self):
+        """Returns string containing ASCII lower case, upper case, and
+        punctuation containers (along with a character space as well)."""
+
         return ''.join(
             char for char in string.printable if char not in '\t\n\r\f\v')
+
 
 if __name__ == '__main__':
     pass
