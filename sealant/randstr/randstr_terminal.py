6e762ce92083ef59e07ec607b5edc67fdd86c754
James <jim-hart@users.noreply.github.com>
2017-09-18 04:50:51 -0400

    Added default character generation limit, along with switch to override that limit
    Imposed a default 1000 character limit on random string generation as strings of excessive length can have negative impact on system resources.  To override this limit, a --remove-limit switch can be provided.  The RandstrOutput._generated_string property determines whether or not this switch is present and then acts accordingly.  If length value exceeding 1000 is provided, but no --remove-limit switch is present, string length defaults to 1000 characters.

--- sealant/randstr/randstr_terminal.py
+++ sealant/randstr/randstr_terminal.py
@@ -50,7 +50,10 @@ class RandstrParser(object):
 
         self.parser.add_argument(
             'len', type=int, metavar='LENGTH',
-            help="Length of the randomized string")
+            help="""Length of the randomized string.  By default, length is \
+            limited by default to 1000 characters unless the --remove-limit \
+            is switch provided. Values exceeding the default limit will be \
+            reduced to 1000 if the --remove-limit switch is not included.""")
 
         # Terminal, clipboard, and file output options
         output_options = self.parser.add_argument_group('Output Options')
@@ -89,10 +92,93 @@ class RandstrParser(object):
             help="""Randomly shuffle character positions in character set prior
             to string generation.""")
 
+        randomization_options.add_argument(
+            '-rl', '--remove-limit', action='store_true', dest='remove_limit',
+            help="""Removes the default 1000 character length limit imposed on \
+            string generation.  Please note strings in excess of ten-thousand \
+            (10K) characters may cause unintended or undesirable \
+            side-effects.""")
+
     @property
     def args(self):
         """:obj:`NameSpace`: User arguments parsed by argparse parser object"""
         return self.parser.parse_args()
+
+
+class RandstrOutput(object):
+    """Class for controlling how the randomly generated string is output
+
+    Attributes:
+        args (obj:`NameSpace`): Arguments parsed by RandstrParser; used as
+            control switches for different output options.
+        generated_string (str): Randomly generated string retrieved through
+            _generated_string property.
+    """
+
+    def __init__(self, parsed_args):
+        self.args = parsed_args
+        self.generated_string = self._generated_string
+
+    @property
+    def _generated_string(self):
+        """str: randomly generated string via RandomString() class."""
+
+        if self.args.remove_limit:
+            str_length = self.args.len
+        else:
+            str_length = self.args.len if self.args.len <= 1000 else 1000
+
+        string_generator = RandomString(
+            length=str_length, shuffle=self.args.shuffle,
+            user_char_set=self.args.characters)
+
+        return string_generator()
+
+    def process_parsed_args(self):
+        """Dispatches output control based on whether or not the --raw-output
+        switch was provided to the argparser."""
+
+        if self.args.raw_output:
+            self.print_raw_output()
+        else:
+            self.print_formatted_output()
+
+    def print_raw_output(self):
+        """Suppresses any kind of output formatting and/or verbose output so
+        only randomly generated string is printed to terminal."""
+
+        sys.stdout.write(self.generated_string)
+
+        if self.args.file:
+            _write_file(self.generated_string, self.args.file)
+
+        if self.args.copy:
+            pyperclip.copy(self.generated_string)
+
+    def print_formatted_output(self):
+        """Displays randomly generated string with additional information like
+        visual delimiters and status messages."""
+
+        print("\n{}Length: {}{}".format(
+            '---------------------------------', len(self.generated_string),
+            '---------------------------------'))
+
+        if self.args.print:
+            print()
+            print(self.generated_string)
+
+        if self.args.file:
+            _write_file(self.generated_string, self.args.file)
+            print("\nOutput written to: {}".format(
+                os.path.abspath(self.args.file)))
+
+        if self.args.copy:
+            pyperclip.copy(self.generated_string)
+            print("\nOutput String copied to clipboard")
+
+        print("\n{}{}".format(
+            '---------------------------------------',
+            '--------------------------------------'))
 
 
 def _write_file(random_string, filename):
@@ -120,78 +206,6 @@ def _generate_filename():
         count += 1
 
     return "randstr_{}.txt".format(count)
-
-
-class RandstrOutput(object):
-    """Class for controlling how the randomly generated string is output
-
-    Attributes:
-        args (obj:`NameSpace`):  Arguments parsed by RandstrParser; used as
-            control switches for different output options.
-     """
-
-    def __init__(self, parsed_args):
-        self.args = parsed_args
-
-    def process_parsed_args(self):
-        """Control method for determining if randomly generated string will be
-        presented in raw or formatted output."""
-
-        string_generator = RandomString(
-            length=self.args.len, shuffle=self.args.shuffle,
-            user_char_set=self.args.characters)
-
-        randomized_string = string_generator()
-
-        if self.args.raw_output:
-            self.print_raw_output(randomized_string)
-        else:
-            self.print_formatted_output(randomized_string)
-
-    def print_raw_output(self, randomized_string):
-        """Suppresses any kind of output formatting and/or verbose output so
-        only randomly generated string is printed to terminal.
-
-        Args:
-            randomized_string (str)
-        """
-
-        sys.stdout.write(randomized_string)
-
-        if self.args.file:
-            _write_file(randomized_string, self.args.file)
-
-        if self.args.copy:
-            pyperclip.copy(randomized_string)
-
-    def print_formatted_output(self, randomized_string):
-        """Displays randomly generated string with additional information like
-        visual delimiters and status messages.
-
-        Args:
-            randomized_string (str)
-        """
-
-        print("\n{}Length: {}{}".format(
-            '---------------------------------', len(randomized_string),
-            '---------------------------------'))
-
-        if self.args.print:
-            print()
-            print(randomized_string)
-
-        if self.args.file:
-            _write_file(randomized_string, self.args.file)
-            print("\nOutput written to: {}".format(
-                os.path.abspath(self.args.file)))
-
-        if self.args.copy:
-            pyperclip.copy(randomized_string)
-            print("\nOutput String copied to clipboard")
-
-        print("\n{}{}".format(
-            '---------------------------------------',
-            '--------------------------------------'))
 
 
 if __name__ == '__main__':
